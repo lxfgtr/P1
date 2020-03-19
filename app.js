616c99e467e58e749ac55dfe464fcd1b3661934b
lxfgtr <62346589+lxfgtr@users.noreply.github.com>
2020-03-19 16:34:39 +0200

    Update app.js
    Am sters com

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 // comentariu
-// alert("JS activated!");
+
 var poze = [	
-"img/PICT2289ed an.JPG", 		//0
-"img/20180608_204203.jpg",		//1
-"img/20180607_144426.jpg",		//2
-"img/20180607_143701.jpg"		//3
+'img/PICT2289ed an.JPG', 		//0
+'img/20180608_204203.jpg',		//1
+'img/20180607_144426.jpg',		//2
+'img/20180607_143701.jpg'		//3
 ];
 
-var btn = document.querySelector("button")
-var img = document.querySelector("img")
+var btn = document.querySelector('button')
+var img = document.querySelector('img')
 var counter = 1
 
-btn.addEventListener("click", function(){
-	if (counter === 4) {counter = 0}
+btn.addEventListener('click', function() {
+	if (counter === 4) {
+		counter = 0;
+	}
 	img.src = poze[counter]
 	counter = counter+1
 });
